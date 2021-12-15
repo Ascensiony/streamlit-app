c4f1341353e27bc5b5559e707dd3d9b5c2a50144
Abhishek Yadav <yadavabhishek0807@gmail.com>
2021-12-15 19:31:00 +0530

    Update streamlit_app.py
    Updated indentations

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,13 @@ import asyncio
 
 from interface import display_matches
 from helpers import load_image, resize, get_random_image_file
-from call_api import call_text_endpoint, call_photo_endpoint, call_hybrid_endpoint, call_greetings_endpoint, call_recommender_api
+from call_api import (
+    call_text_endpoint,
+    call_photo_endpoint,
+    call_hybrid_endpoint,
+    call_greetings_endpoint,
+    call_recommender_api,
+)
 
 app_formal_name = "Image Search Engine"
 
@@ -29,9 +35,6 @@ def main():
     )
 
     st.sidebar.markdown(f"{app_formal_name}")
-    # st.sidebar.markdown(
-    #     f"Images are sourced from the [Unsplash dataset](https://github.com/unsplash/datasets)"
-    # )
 
     DEFAULT_ENDPOINT = st.text_input("Endpoint", DEFAULT_ENDPOINT)
 
@@ -53,30 +56,32 @@ def textq_search():
     reformulated_queries = []
 
     with st.form("text_search_form"):
-        text_query = st.text_input(label='Enter the query', max_chars=70)
-        k = st.slider("Number of Images to retrieve",
-                      min_value=1, max_value=20, value=6)
+        text_query = st.text_input(label="Enter the query", max_chars=70)
+        k = st.slider(
+            "Number of Images to retrieve", min_value=1, max_value=20, value=6
+        )
         submitted = st.form_submit_button("Search")
         if submitted:
-            reformulated_queries = asyncio.run(
-                call_recommender_api(text_query))
+            reformulated_queries = asyncio.run(call_recommender_api(text_query))
 
-            with st.spinner('Searching'):
+            with st.spinner("Searching"):
                 response = call_text_endpoint(text_query, DEFAULT_ENDPOINT)
 
             if response.ok:
-                matching_ids = response.json()['image_ids'][:k]
+                matching_ids = response.json()["image_ids"][:k]
 
             else:
                 st.error(
-                    f'Requests Not Successful, Code: {response.status_code}, Reason: {response.reason}')
+                    f"Requests Not Successful, Code: {response.status_code}, Reason: {response.reason}"
+                )
 
     if submitted:
         st.markdown("***")
         st.balloons()
         if reformulated_queries:
             st.markdown(
-                f"""**People also search for:** `{"` `".join(reformulated_queries)}`""")
+                f"""**People also search for:** `{"` `".join(reformulated_queries)}`"""
+            )
             # st.markdown(f"""`{"`         `".join(reformulated_queries)}`""")
         display_matches(matching_ids, css0, css1)
 
@@ -91,32 +96,34 @@ def imageq_search():
         col1, col2 = st.columns(2)
 
         with col1:
-            image = load_image(
-                ROOT_DIR + get_random_image_file("./cached_images"))
+            image = load_image(ROOT_DIR + get_random_image_file("./cached_images"))
 
             uploaded_image = st.file_uploader(
-                "or upload an image", type=["jpg", "jpeg", "png"])
+                "or upload an image", type=["jpg", "jpeg", "png"]
+            )
             if uploaded_image is not None:
                 image = load_image(uploaded_image)
 
         with col2:
             st.image(resize(image, 400))
 
-        k = st.slider("Number of Images to retrieve",
-                      min_value=1, max_value=20, value=6)
+        k = st.slider(
+            "Number of Images to retrieve", min_value=1, max_value=20, value=6
+        )
 
         # Every form must have a submit button.
         submitted = st.form_submit_button("Search")
         if submitted:
-            with st.spinner('Searching'):
+            with st.spinner("Searching"):
                 response = call_photo_endpoint(image, DEFAULT_ENDPOINT)
 
             if response.ok:
-                matching_ids = response.json()['image_ids'][:k]
+                matching_ids = response.json()["image_ids"][:k]
 
             else:
                 st.error(
-                    f'Requests Not Successful, Code: {response.status_code}, Reason: {response.reason}')
+                    f"Requests Not Successful, Code: {response.status_code}, Reason: {response.reason}"
+                )
 
     if submitted:
         st.markdown("***")
@@ -134,41 +141,45 @@ def hybridq_search():
         col1, col2 = st.columns(2)
 
         with col1:
-            image = load_image(
-                ROOT_DIR + get_random_image_file("./cached_images"))
+            image = load_image(ROOT_DIR + get_random_image_file("./cached_images"))
 
             uploaded_image = st.file_uploader(
-                "or upload an image", type=["jpg", "jpeg", "png"])
+                "or upload an image", type=["jpg", "jpeg", "png"]
+            )
             if uploaded_image is not None:
                 image = load_image(uploaded_image)
 
         with col2:
             st.image(resize(image, 400))
 
-        text_query = st.text_input(label='Enter the query', max_chars=70)
+        text_query = st.text_input(label="Enter the query", max_chars=70)
         col3, col4 = st.columns(2)
 
         with col3:
-            k = st.slider("Number of Images to retrieve",
-                          min_value=1, max_value=3, value=3)
+            k = st.slider(
+                "Number of Images to retrieve", min_value=1, max_value=3, value=3
+            )
 
         with col4:
-            image_weight = st.slider("Photo Weight",
-                                     min_value=0.0, max_value=1.0, value=0.5, step=0.01)
+            image_weight = st.slider(
+                "Photo Weight", min_value=0.0, max_value=1.0, value=0.5, step=0.01
+            )
 
         # Every form must have a submit button.
         submitted = st.form_submit_button("Search")
         if submitted:
-            with st.spinner('Searching'):
+            with st.spinner("Searching"):
                 response = call_hybrid_endpoint(
-                    text_query, image, image_weight, DEFAULT_ENDPOINT)
+                    text_query, image, image_weight, DEFAULT_ENDPOINT
+                )
 
             if response.ok:
-                matching_ids = response.json()['image_ids'][:k]
+                matching_ids = response.json()["image_ids"][:k]
 
             else:
                 st.error(
-                    f'Requests Not Successful, Code: {response.status_code}, Reason: {response.reason}')
+                    f"Requests Not Successful, Code: {response.status_code}, Reason: {response.reason}"
+                )
 
     if submitted:
         st.markdown("***")
@@ -183,7 +194,8 @@ def api_test():
             st.success("heYY!! Greetings from the API")
         else:
             st.error(
-                f'Awww the api is unavailable right now, Code: {response.status_code}, Reason: {response.reason}')
+                f"Awww the api is unavailable right now, Code: {response.status_code}, Reason: {response.reason}"
+            )
 
 
 if __name__ == "__main__":
